b6f74fa7e1d56abe983e6f08e997e27bae851cd1
Whaskalmanik <whakalman@gmail.com>
2019-12-08 01:38:39 +0100

    Login activity

--- app/src/main/java/com/whaskalmanik/dtssensor/Entry.java
+++ app/src/main/java/com/whaskalmanik/dtssensor/Entry.java
@@ -1,4 +1,16 @@
 package com.whaskalmanik.dtssensor;
 
 public class Entry {
+    public final String delka;
+    public final String teplota;
+    public final String stokes;
+    public final String antistokes;
+
+    Entry(String delka,String teplota, String stokes, String antistokes) {
+        this.delka=delka;
+        this.teplota=teplota;
+        this.stokes=stokes;
+        this.antistokes=antistokes;
+    }
 }
+
--- app/src/main/java/com/whaskalmanik/dtssensor/SettingsActivity.java
+++ app/src/main/java/com/whaskalmanik/dtssensor/SettingsActivity.java
@@ -2,28 +2,22 @@ package com.whaskalmanik.dtssensor;
 
 
 import android.content.SharedPreferences;
-
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+
 /**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
+ * This preference activity has in its manifest declaration an intent filter for
+ * the ACTION_MANAGE_NETWORK_USAGE action. This activity provides a settings UI
+ * for users to specify network settings to control data usage.
  */
 public class SettingsActivity extends PreferenceActivity
         implements
-        SharedPreferences.OnSharedPreferenceChangeListener {
+        OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Loads the XML preferences file.
         addPreferencesFromResource(R.xml.preferences);
     }
@@ -52,6 +46,6 @@ public class SettingsActivity extends PreferenceActivity
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         // Sets refreshDisplay to true so that when the user returns to the main
         // activity, the display refreshes to reflect the new settings.
-        MainActivity.refreshDisplay = true;
+        NetworkActivity.refreshDisplay = true;
     }
 }
