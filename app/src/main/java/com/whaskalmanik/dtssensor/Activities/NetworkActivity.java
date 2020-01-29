c350ecc1118271e676cdd81a70ea9deaff25c1fd
Whaskalmanik <whakalman@gmail.com>
2020-01-29 22:56:42 +0100

    Removed unneeded activities (New Start)

--- app/src/main/java/com/whaskalmanik/dtssensor/Activities/NetworkActivity.java
+++ app/src/main/java/com/whaskalmanik/dtssensor/Activities/NetworkActivity.java
@@ -3,10 +3,6 @@ package com.whaskalmanik.dtssensor.Activities;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentContainer;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -25,21 +21,10 @@ import android.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-import com.whaskalmanik.dtssensor.Database.DatabaseHelper;
-import com.whaskalmanik.dtssensor.Entry;
-import com.whaskalmanik.dtssensor.Fragments.AntistokesFragment;
-import com.whaskalmanik.dtssensor.Fragments.StokesFragment;
-import com.whaskalmanik.dtssensor.Fragments.TemperatureFragment;
 import com.whaskalmanik.dtssensor.R;
-import com.whaskalmanik.dtssensor.XMLParser;
 
 import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
@@ -63,35 +48,7 @@ import java.util.Objects;
  *   to refresh the WebView content.
  */
 public class NetworkActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    public static final String WIFI = "Wi-Fi";
-    public static final String ANY = "Any";
-
-    private static final String URL =
-            "https://homel.vsb.cz/~ohe0004/Teploty.xml";
-
-
-    // Whether there is a Wi-Fi connection.
-    private static boolean wifiConnected = false;
-    // Whether there is a mobile connection.
-    private static boolean mobileConnected = false;
-    // Whether the display should be refreshed.
-    public static boolean refreshDisplay = true;
-
-    // The user's current network preference setting.
-    public static String sPref = null;
-
-    // The BroadcastReceiver that tracks network connectivity changes.
-    private NetworkReceiver receiver = new NetworkReceiver();
-
     private DrawerLayout drawer;
-    DatabaseHelper db;
-    String email;
-
-    double[] delka;
-    double[] stokes;
-    double[] antistokes;
-    double[] teplota;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -101,45 +58,18 @@ public class NetworkActivity extends AppCompatActivity implements NavigationView
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        email = Objects.requireNonNull(getIntent().getExtras()).getString("email");
-
         drawer = findViewById(R.id.drawer_layout);
         setHeader(savedInstanceState);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        // Register BroadcastReceiver to track connection changes.
-        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        receiver = new NetworkReceiver();
-        this.registerReceiver(receiver, filter);
     }
 
     public void setHeader(Bundle savedInstanceState) {
-        db = new DatabaseHelper(this);
-        Cursor c = db.select(email);
-        c.moveToLast();
-        String name = c.getString(2);
-        String surname = c.getString(3);
-        StringBuilder b = new StringBuilder();
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        if (savedInstanceState == null) {
-            if(delka!=null||teplota!=null)
-            {
-                TemperatureFragment tmp=TemperatureFragment.newInstance(delka,teplota);
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, tmp).commit();
-                navigationView.setCheckedItem(R.id.tempterature);
-            }
-        }
-        b.append(name).append(" ").append(surname);
-        View headView = navigationView.getHeaderView(0);
-        TextView n = headView.findViewById(R.id.headName);
-        TextView em = headView.findViewById(R.id.headEmail);
-        em.setText(email);
-        n.setText(b.toString());
     }
 
     @Override
@@ -155,51 +85,24 @@ public class NetworkActivity extends AppCompatActivity implements NavigationView
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.tempterature: {
-                if(delka==null||teplota==null)
-                {
-                    break;
-                }
-                TemperatureFragment fragment=TemperatureFragment.newInstance(delka,teplota);
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                 break;
             }
             case R.id.stokes: {
-                if(delka==null||stokes==null)
-                {
-                    break;
-                }
-                StokesFragment fragment=StokesFragment.newInstance(delka,stokes);
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                 break;
-
             }
             case R.id.antistokes: {
-                if(delka==null||antistokes==null)
-                {
-                    break;
-                }
-                AntistokesFragment fragment=AntistokesFragment.newInstance(delka,antistokes);
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
                 break;
             }
             case R.id.psChange: {
-                Intent psChange = new Intent(this, ChangePasswordActivity.class);
-                psChange.putExtra("email", email);
-                startActivity(psChange);
                 break;
             }
             case R.id.logOut: {
-                Toast.makeText(getApplicationContext(), "Login out", Toast.LENGTH_SHORT).show();
-                finish();
                 break;
             }
             case R.id.settings: {
-                Intent settings = new Intent(this, SettingsActivity.class);
-                startActivity(settings);
                 break;
             }
             case R.id.refresh: {
-                loadPage();
                 break;
             }
         }
@@ -209,188 +112,5 @@ public class NetworkActivity extends AppCompatActivity implements NavigationView
 
     // Refreshes the display if the network connection and the
     // pref settings allow it.
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // Gets the user's network preference settings
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-        // Retrieves a string value for the preferences. The second parameter
-        // is the default value to use if a preference value is not found.
-        sPref = sharedPrefs.getString("listPref", "Wi-Fi");
-
-        updateConnectedFlags();
-
-        // Only loads the page if refreshDisplay is true. Otherwise, keeps previous
-        // display. For example, if the user has set "Wi-Fi only" in prefs and the
-        // device loses its Wi-Fi connection midway through the user using the app,
-        // you don't want to refresh the display--this would force the display of
-        // an error page instead of stackoverflow.com content.
-        if (refreshDisplay) {
-            loadPage();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (receiver != null) {
-            this.unregisterReceiver(receiver);
-        }
-    }
-
-    // Checks the network connection and sets the wifiConnected and mobileConnected
-    // variables accordingly.
-    private void updateConnectedFlags() {
-        ConnectivityManager connMgr =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
-        if (activeInfo != null && activeInfo.isConnected()) {
-            wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
-            mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
-        } else {
-            wifiConnected = false;
-            mobileConnected = false;
-        }
-    }
-
-    // Uses AsyncTask subclass to download the XML feed from stackoverflow.com.
-    // This avoids UI lock up. To prevent network operations from
-    // causing a delay that results in a poor user experience, always perform
-    // network operations on a separate thread from the UI.
-    private void loadPage() {
-        if (((sPref.equals(ANY)) && (wifiConnected || mobileConnected))
-                || ((sPref.equals(WIFI)) && (wifiConnected))) {
-            // AsyncTask subclass
-            new DownloadXmlTask(this).execute(URL);
-        } else {
-            showErrorPage();
-        }
-    }
-
-    // Displays an error if the app is unable to load content.
-    private void showErrorPage() {
-        Toast.makeText(getApplicationContext(),"Error downloading file",Toast.LENGTH_LONG).show();
-    }
-
-    // Implementation of AsyncTask used to download XML feed from stackoverflow.com.
-    private class DownloadXmlTask extends AsyncTask<String, Void, List<Entry>> {
-
-        Context context;
-
-        public DownloadXmlTask(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        protected List<Entry> doInBackground(String... urls) {
-            try {
-                return loadXmlFromNetwork(urls[0]);
-            } catch (IOException e) {
-                return null; //getResources().getString(R.string.connection_error);
-            } catch (XmlPullParserException e) {
-                return null; //getResources().getString(R.string.xml_error);
-            }
-        }
-
-        @Override
-        protected void onPostExecute(List<Entry> result) {
-            Toast.makeText(getApplicationContext(),"Data downloaded",Toast.LENGTH_SHORT).show();
-            delka=new double[result.size()];
-            stokes=new double[result.size()];
-            antistokes=new double[result.size()];
-            teplota=new double[result.size()];
-            for (int i=0;i<result.size();i++)
-            {
-
-                delka[i]=Double.parseDouble((result.get(i).delka));
-                stokes[i]=Double.parseDouble((result.get(i).stokes));
-                antistokes[i]= Double.parseDouble((result.get(i).antistokes));
-                teplota[i]=Double.parseDouble((result.get(i).teplota));
-            }
-        }
-    }
-
-    // Uploads XML from stackoverflow.com, parses it, and combines it with
-    // HTML markup. Returns HTML string.
-    private List<Entry> loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
-        InputStream stream = null;
-        XMLParser cnbXmlParser = new XMLParser();
-        List<Entry> entries = null;
-
-        try {
-            stream = downloadUrl(urlString);
-            entries = cnbXmlParser.parse(stream);
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
-        }
-
-
-        return entries;
-    }
-
-    // Given a string representation of a URL, sets up a connection and gets
-    // an input stream.
-    private InputStream downloadUrl(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(10000 /* milliseconds */);
-        conn.setConnectTimeout(15000 /* milliseconds */);
-        conn.setRequestMethod("GET");
-        conn.setDoInput(true);
-        // Starts the query
-        conn.connect();
-        InputStream stream = conn.getInputStream();
-        return stream;
-    }
-
-    /**
-     *
-     * This BroadcastReceiver intercepts the android.net.ConnectivityManager.CONNECTIVITY_ACTION,
-     * which indicates a connection change. It checks whether the type is TYPE_WIFI.
-     * If it is, it checks whether Wi-Fi is connected and sets the wifiConnected flag in the
-     * main activity accordingly.
-     *
-     */
-    public class NetworkReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connMgr =
-                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-            // Checks the user prefs and the network connection. Based on the result, decides
-            // whether
-            // to refresh the display or keep the current display.
-            // If the userpref is Wi-Fi only, checks to see if the device has a Wi-Fi connection.
-            if (WIFI.equals(sPref) && networkInfo != null
-                    && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                // If device has its Wi-Fi connection, sets refreshDisplay
-                // to true. This causes the display to be refreshed when the user
-                // returns to the app.
-                refreshDisplay = true;
-                Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
-
-                // If the setting is ANY network and there is a network connection
-                // (which by process of elimination would be mobile), sets refreshDisplay to true.
-            } else if (ANY.equals(sPref) && networkInfo != null) {
-                refreshDisplay = true;
-
-                // Otherwise, the app can't download content--either because there is no network
-                // connection (mobile or Wi-Fi), or because the pref setting is WIFI, and there
-                // is no Wi-Fi connection.
-                // Sets refreshDisplay to false.
-            } else {
-                refreshDisplay = false;
-                Toast.makeText(context, R.string.lost_connection, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 }
